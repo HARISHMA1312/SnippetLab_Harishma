a1e232a2a573db728b3735c6946e8df73bd9864f
HARISHMA1312 <harishmad@student.tce.edu>
2024-09-28 18:02:16 +0530

    Initial commit

--- src/codemirror-5.65.17/mode/python/python.js
+++ src/codemirror-5.65.17/mode/python/python.js
@@ -1,5 +1,6 @@
 // CodeMirror, copyright (c) by Marijn Haverbeke and others
 // Distributed under an MIT license: https://codemirror.net/5/LICENSE
+/* global define, CodeMirror */
 
 (function(mod) {
   if (typeof exports == "object" && typeof module == "object") // CommonJS
